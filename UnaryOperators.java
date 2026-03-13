3f87dd787ff6446d1d9c02f3dddf30e7af0d5157
S Chetas <chetaschintu123@gmail.com>
2026-03-13 10:51:00 +0530

    Add files via upload

---
+++ UnaryOperators.java
@@ -0,0 +1,12 @@
+//Unary Operators:Operators that operate on a single operand
+class Main
+{
+    static public void main(String []args)
+    {
+        int a=5,b=10;
+        System.out.println("Pre Increment: "+ (++a));
+        System.out.println("Post Increment: "+ (a++));
+        System.out.println("Pre Decrement: "+ (--b));
+        System.out.println("Post Decrement: "+ (b--));
+    }
+}
