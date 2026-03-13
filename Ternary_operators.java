b1272ba2f6f9c5f43a93204f2f466d9a1437a80c
S Chetas <chetaschintu123@gmail.com>
2026-03-13 11:55:31 +0530

    Add files via upload
    Ternary Operators In Java

---
+++ Ternary_operators.java
@@ -0,0 +1,9 @@
+//Ternary Operator: works on 3 operands,shorthand version of if-else statement
+class Main
+{
+    static public void main(String []chetas){
+        int a=10,b=5;
+        String result=(a>b)?"a is greater":"b is greater";
+        System.out.println(result);
+    }
+}
