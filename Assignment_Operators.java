fa880477037e964917174330952e9b79722068ca
S Chetas <chetaschintu123@gmail.com>
2026-03-13 11:04:22 +0530

    Add files via upload
    Assignment Operators in java

---
+++ Assignment_Operators.java
@@ -0,0 +1,19 @@
+//Assignment Operator(=): used to assign a value to a variable
+class Main 
+{
+    static public void main(String[] chetas)
+    {
+        int a=5;
+        a+=5;
+        System.out.println(a);
+        a-=5;
+        System.out.println(a);
+        a*=5;
+        System.out.println(a);
+        a/=5;
+        System.out.println(a);
+        a%=5;
+        System.out.println(a);
+        
+    }
+}
