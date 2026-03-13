e48121f8bf6b902948eb9910961caba3fc1c4670
S Chetas <chetaschintu123@gmail.com>
2026-03-13 11:16:53 +0530

    Add files via upload
    Relational Operators In Java

---
+++ Relational operators.java
@@ -0,0 +1,15 @@
+//Relational operators: These are the Comparision Operators.Returns a boolean value
+class Main
+{
+    static public void main(String[] chetas)
+    {
+        int a=5,b=8;
+        System.out.println("a>b: "+(a>b));
+        System.out.println("a>=b: "+(a>=b));
+        System.out.println("a<b: "+(a<b));
+        System.out.println("a<=b: "+(a<=b));
+        System.out.println("a==b: "+(a==b));
+        System.out.println("a!=b: "+(a!=b));
+        
+    }
+}
