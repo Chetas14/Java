09392a70903c56e46a960bd569f9e29c057c0404
S Chetas <chetaschintu123@gmail.com>
2026-03-13 10:38:25 +0530

    Add files via upload
    Demonstration of arithmetic operators in Java

---
+++ Arithmetic_Operators.java
@@ -0,0 +1,21 @@
+//Operators:Special Symbols that performs operations on variables or values
+//Arithmetic Operators:used to perform simple arithmetic operations on primitive and Non-primitive datatypes
+//Example of Arithmetic operators: +,-,*,/,%
+class Main
+{
+    static public void main(String[] chetas)
+    {
+        int a=100,b=9;
+        int add=a+b;//Performs addition;
+        int sub=a-b;//Performs subtraction
+        int mul=a*b;//Performs multiplication
+        double div=a/b;//Performs division
+        int rem=a%b;
+        System.out.println("Addition: "+add);
+        System.out.println("Subtraction: "+sub);
+        System.out.println("Multiplication: "+mul);
+        System.out.println("Division: "+div);
+        System.out.println("Modulus: "+rem);
+        
+    }
+}
